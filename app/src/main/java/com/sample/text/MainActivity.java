d86269b945afce0121aea0dcd204d469d28a05bd
Hanselord28 <Hanselord16@gmail.com>
2025-10-23 22:02:27 -0300

    3rd

--- app/src/main/java/com/sample/text/MainActivity.java
+++ app/src/main/java/com/sample/text/MainActivity.java
@@ -1,18 +1,34 @@
 package com.sample.text;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAnalytics oFirebaseAnalytics;
+    private FirebaseAuth oFirebaseAuth;
+    private static final String TAG = "EmailPassword";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "pene", Toast.LENGTH_SHORT).show();
+        oFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("Mensaje", "Entro_al_Log");
+        oFirebaseAnalytics.logEvent("pantalla_de_logeo", bundle);
+        this.setTitle("Inicio de Sesión");
+
     }
 }
